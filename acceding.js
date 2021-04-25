c5015130d3463d86f56b729e34598ae3d94e7fe1
shreefaw <shreefaw@gmail.com>
2021-04-25 08:22:43 +0300

    editing if statements

--- acceding.js
+++ acceding.js
@@ -16,12 +16,9 @@ let a = +(prompt("First Number")) || 0;
 let b = +(prompt("Second Number")) || 0;
 let c = +(prompt("Third Number")) || 0;
 if (a < c) {
-  if (a == b) {
+  if (a <= b && b<c) {
       alert([a,b,c]);
   } 
-  else if (a < b && b < c) {
-      alert([a,b,c]);
-  }
   else if (b < a){
       alert([b,a,c]);
   }
@@ -30,32 +27,20 @@ if (a < c) {
   }
 } 
 else if (b < a){
-    if (b == c){
-        alert([b,c,a]);
-    }
-    else if (b<c && c<a){
+    if (b <= c && c<a){
         alert([b,c,a]);
     }
     else if (c<b){
         alert([c,b,a]);
     }
-    else{
-        alert([b,a,c]);
-    }
+    
 }
 else if (c < b){
-    if(c == a){
+    if(c <= a && a<b){
         alert([c,a,b]);
     }
-    else if (c< a && a<b){
-        alert([c,a,b]);
-    }
-    else if (a<c){
-        alert([a,c,b]);
-    }
-    else {
-        alert([c,b,a]); 
-    }
+    
+    
 }
 else{
     alert([a,b,c]);
